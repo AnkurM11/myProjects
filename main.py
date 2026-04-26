7fd0d6573bcdeabbed5312de0cc270af9617f20b
AnkurM11 <ankurmadhukar377@gmail.com>
2026-04-26 19:04:22 +0530

    Day-1 : Setup Initalisation Configuration

---
+++ main.py
@@ -0,0 +1,11 @@
+##job market analyzer
+##day 1 - basic setup
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+print("Job Market Intialiser -- Starting")
+print("Libraries Loaded Succesfully")
+print("Ready to analyse job Market Data.")
